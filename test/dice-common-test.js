c634569e98b5b15bf2bdcbbd6d0856a677d52a88
Valerie <valerie@quantumpines.com>
2021-09-26 15:15:09 -0700

    prepping for parse and eval

--- test/dice-common-test.js
+++ test/dice-common-test.js
@@ -1,5 +1,5 @@
 const test = require('ava');
-const { Metadata, RollMetadata, _RollMetadata } = require('../lib/dice-common');
+const { RollMetadata, _RollMetadata } = require('../lib/dice-common');
 
 test('my passing test', t => {
   t.pass();
@@ -7,5 +7,10 @@ test('my passing test', t => {
 
 test('[2,1]', t => {
   const md = new RollMetadata([2,1]);
-  t.deepEqual( md, new _RollMetadata([1,2], [1,2], 3) );
+  t.deepEqual( md, new _RollMetadata("Roll", [1,2], [1,2], 3) );
+});
+
+test('[3,1]', t => {
+  const md = new RollMetadata([3,1], '>');
+  t.deepEqual( md, new _RollMetadata("Roll", [1,3], [3], 3) );
 });
--- test/parser-test.js
+++ test/parser-test.js
@@ -1,37 +1,37 @@
 const test = require('ava');
 const DiceParser = require('../lib/parser.js');
-const { Metadata } = require('../lib/dice-common');
+const { DiceMetadata, _RollMetadata } = require('../lib/dice-common');
 
 test('1d6', t => {
   var metadata = DiceParser.ParseDie('1d6');
 
-  t.deepEqual(metadata, new Metadata(1, 'd', 6));
+  t.deepEqual(metadata, new DiceMetadata(1, 'd', 6));
 });
 
 test('1D6', t => {
   var metadata = DiceParser.ParseDie('1D6');
 
-  t.deepEqual(metadata, new Metadata(1, 'D', 6) );
+  t.deepEqual(metadata, new DiceMetadata(1, 'D', 6) );
 });
 
 test('2d6', t => {
   var metadata = DiceParser.ParseDie('2d6');
 
-  t.deepEqual(metadata, new Metadata(2, 'd', 6) );
+  t.deepEqual(metadata, new DiceMetadata(2, 'd', 6) );
 });
 
 test('2d6>', t => {
   var metadata = DiceParser.ParseDie('2d6>');
 
-  t.deepEqual(metadata, new Metadata(2, 'd', 6, '>') );
+  t.deepEqual(metadata, new DiceMetadata(2, 'd', 6, '>') );
 });
 
 test('1d8 + 1d6', t=> {
   var metadata = DiceParser.ParseAll('1d8 + 1d6');
 
   t.deepEqual(metadata,
-    [new Metadata(1, 'd', 8),
-    '+',
-    new Metadata(1, 'd', 6)]
+    [new DiceMetadata(1, 'd', 8),
+    new _RollMetadata("Op", undefined, undefined, undefined, '+'),
+    new DiceMetadata(1, 'd', 6)]
   );
 });
