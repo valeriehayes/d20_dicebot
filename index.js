20da4bdfa523a5921b307d0b5d1f557ba8a58847
Valerie <valerie@quantumpines.com>
2021-09-26 15:50:10 -0700

    add sinon dependency. moved RollDice and rolldie.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const config = require("./config.json");
 const { Client, Intents } = require("discord.js");
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const DiceParser = require("./lib/parser.js");
+const {RollDice} = require("./lib/dice-common.js");
 
 client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -35,7 +36,7 @@ client.on("messageCreate", (msg) => {
     console.log(diceMetadata);
 
     if (diceMetadata) {
-      const results = DiceParser.RollDice(diceMetadata);
+      const results = RollDice(diceMetadata);
       console.log(results);
 
       const prettyPrint = PrettyPrint(diceMetadata, results);
