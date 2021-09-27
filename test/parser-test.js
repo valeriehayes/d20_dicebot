f3b0756c594ca74fba6097c08a7ba26c75d30596
Valerie <valerie@quantumpines.com>
2021-09-27 01:07:41 -0700

    fix tests

--- test/parser-test.js
+++ test/parser-test.js
@@ -7,33 +7,33 @@ const { DiceGroup } = require('../lib/dice-common');
 test('1d6', t => {
   var metadata = DiceParser.ParseDie('1d6');
 
-  t.deepEqual(metadata, new DiceGroup(1, 'd', 6));
+  t.deepEqual(metadata, new DiceGroup(1, 'd', 6, null, "1d6"));
 });
 
 test('1D6', t => {
   var metadata = DiceParser.ParseDie('1D6');
 
-  t.deepEqual(metadata, new DiceGroup(1, 'D', 6) );
+  t.deepEqual(metadata, new DiceGroup(1, 'D', 6, null, "1D6") );
 });
 
 test('2d6', t => {
   var metadata = DiceParser.ParseDie('2d6');
 
-  t.deepEqual(metadata, new DiceGroup(2, 'd', 6) );
+  t.deepEqual(metadata, new DiceGroup(2, 'd', 6, null, "2d6") );
 });
 
 test('2d6>', t => {
   var metadata = DiceParser.ParseDie('2d6>');
 
-  t.deepEqual(metadata, new DiceGroup(2, 'd', 6, '>') );
+  t.deepEqual(metadata, new DiceGroup(2, 'd', 6, '>', "2d6>") );
 });
 
 test('1d8 + 1d6', t => {
   var metadata = DiceParser.ParseAll('1d8 + 1d6');
 
   t.deepEqual(metadata,
-    [new DiceGroup(1, 'd', 8),
-    new DiceGroup(1, 'd', 6)]
+    [new DiceGroup(1, 'd', 8, null, "1d8"),
+    new DiceGroup(1, 'd', 6, null, "1d6")]
   );
 });
 
@@ -41,7 +41,7 @@ test('1d20', t => {
   var metadata = DiceParser.ParseAll('1d20');
 
   t.deepEqual(metadata,
-    [new DiceGroup(1, 'd', 20)]
+    [new DiceGroup(1, 'd', 20, null, "1d20")]
   );
 });
 
@@ -49,6 +49,6 @@ test('2d20>', t => {
   var metadata = DiceParser.ParseAll('2d20>');
 
   t.deepEqual(metadata,
-    [new DiceGroup(2, 'd', 20, '>')]
+    [new DiceGroup(2, 'd', 20, '>', "2d20>")]
   );
 });
