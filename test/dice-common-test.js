3a5a507a80bb79ad8c2a9c89dc52507e71eee923
Valerie <valerie@quantumpines.com>
2021-09-26 17:14:05 -0700

    rename and add test example for future use.

--- test/dice-common-test.js
+++ test/dice-common-test.js
@@ -2,12 +2,12 @@ const test = require('ava');
 const DiceCommons = require('../lib/dice-common');
 const sinon = require("sinon");
 
-var RollMetadata;
+var ProcessRolls;
 var _RollMetadata;
 var DiceMetadata
 
 test.before( t => {
-  RollMetadata = DiceCommons.RollMetadata;
+  ProcessRolls = DiceCommons.ProcessRolls;
   _RollMetadata = DiceCommons._RollMetadata;
   DiceMetadata = DiceCommons.DiceMetadata;
 });
@@ -17,12 +17,12 @@ test('my passing test', t => {
 });
 
 test('[2,1]', t => {
-  const md = new RollMetadata([2,1]);
+  const md = new ProcessRolls([2,1]);
   t.deepEqual( md, new _RollMetadata("Roll", [1,2], [1,2], 3) );
 });
 
 test('[3,1]', t => {
-  const md = new RollMetadata([3,1], '>');
+  const md = new ProcessRolls([3,1], '>');
   t.deepEqual( md, new _RollMetadata("Roll", [1,3], [3], 3) );
 });
 
@@ -41,6 +41,19 @@ test('mock rolldie()', t => {
 
   var retval = DiceCommons.RollDice(new DiceMetadata(1, 'd', 6));
   t.deepEqual(retval, new _RollMetadata("Roll", [8], [8], 8) );
+
+  sinon.restore();
+});
+
+test('roll 2d6', t => {
+  var callback = sinon.stub();
+  callback.onCall(0).returns(2);
+  callback.onCall(1).returns(4);
+  sinon.replace(Math, "floor", callback);
+  // sinon.replace(DiceCommons, "_rolldie", callback);
+
+  var retval = DiceCommons.RollDice(new DiceMetadata(2, 'd', 6, undefined));
+  t.deepEqual(retval, new _RollMetadata("Roll", [3, 5], [3, 5], 8));
 
   sinon.restore();
 });
